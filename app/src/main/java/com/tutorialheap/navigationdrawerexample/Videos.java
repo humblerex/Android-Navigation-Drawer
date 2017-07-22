0a8cc23965e0b7ef8f8ea3e1af07b0305ed018f9
Humble Rex <ashkumar658@gmail.com>
2017-07-22 22:22:59 +0530

    Android Navigation Drawer

---
+++ app/src/main/java/com/tutorialheap/navigationdrawerexample/Videos.java
@@ -0,0 +1,30 @@
+       package com.tutorialheap.navigationdrawerexample;
+
+       import android.os.Bundle;
+       import android.support.annotation.Nullable;
+       import android.support.v4.app.Fragment;
+       import android.view.LayoutInflater;
+       import android.view.View;
+       import android.view.ViewGroup;
+
+
+       /**
+        * Created by A.D. on 22/07/17.
+        */
+
+
+       public class Videos extends Fragment {
+
+           @Nullable
+           @Override
+           public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+                return inflater.inflate(R.layout.videos_fragment, container, false);
+           }
+
+
+           @Override
+           public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+               super.onViewCreated(view, savedInstanceState);
+               getActivity().setTitle("Videos");
+           }
+       }
